236183380b8c8eae71bd1750bd32d985f8f994c0
Alex Lee <hyunho98@gmail.com>
2019-05-03 22:13:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = new Array();
+
+  for (var i = 0; i > musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+
+  return array;
+}
